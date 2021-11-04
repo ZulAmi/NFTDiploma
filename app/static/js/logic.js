cde52e6e3d828f9722bbdd876df858ae8a34b0ef
ZulAmi <92867326+ZulAmi@users.noreply.github.com>
2021-11-04 18:49:52 +0800

    Add files via upload

--- app/static/js/logic.js
+++ app/static/js/logic.js
@@ -1,13 +1,8 @@
-Moralis.initialize("U9V9gcbRgcyJJtYI4ExK0I11qiPgyaBj0Kzmq0Pj"); // Application id from moralis.io
-Moralis.serverURL = "https://wuhkfpsnsfh4.usemoralis.com:2053/server"; //Server url from moralis.io
+Moralis.initialize("U9V9gcbRgcyJJtYI4ExK0I11qiPgyaBj0Kzmq0Pj");
+Moralis.serverURL = "https://wuhkfpsnsfh4.usemoralis.com:2053/server";
 
-const nft_contract_address = "0x88624DD1c725C6A95E223170fa99ddB22E1C6DDD" //NFT Minting Contract Use This One "Batteries Included", code of this contract is in the github repository under contract_base for your reference.
-/*
-Available deployed contracts
-Ethereum Rinkeby 0x0Fb6EF3505b9c52Ed39595433a21aF9B5FCc4431
-Polygon Mumbai 0x351bbee7C6E9268A1BF741B098448477E08A0a53
-BSC Testnet 0x88624DD1c725C6A95E223170fa99ddB22E1C6DDD
-*/
+const nft_contract_address = "0x0Fb6EF3505b9c52Ed39595433a21aF9B5FCc4431"
+
 
 const web3 = new Web3(window.ethereum);
 
